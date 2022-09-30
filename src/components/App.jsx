b7dd7329c5dcb230009fc58a3fc50e2ba0085370
nadtosonna <prasolova.kateryna@gmail.com>
2022-10-01 01:36:16 +0300

    feedback rework finished

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,59 +1,58 @@
-import { Component } from "react";
+import { useState } from "react";
 import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
 import Notification from "./Notification/Notification";
 import Section from "./Section/Section";
 import Statistics from "./Statistics/Statistics";
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  };
+export function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  onLeaveFeedback = name => {
-    this.setState(prevState => {
-      return {
-        [name]: prevState[name] + 1,
-      };
-    });
-  };
-
-  countTotal() {
-    const { good, neutral, bad } = this.state;
+  const countTotal = () => {
     return good + neutral + bad;
   }
 
-  countPositive() {
-    const total = this.countTotal();
+  const total = countTotal();
+
+  const countPositive = () => {
     if (!total) {
       return 0;
     }
-    const { good } = this.state;
     const result = Math.round((good / (total)) * 100);
     return result;
   }
 
-  render() {
-      return (
-        <div>
-          <Section title="Please leave your feedback:">
-            <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.onLeaveFeedback} />
-          </Section>
-          <Section title="Statistics:">
-            {this.countTotal() ?
-              <Statistics
-                good={this.state.good}
-                neutral={this.state.neutral}
-                bad={this.state.bad}
-                total={this.countTotal()}
-                positiveFeedback={this.countPositive()} /> :
-              <Notification message="There is no feedback yet." />}
-            </Section>
-        </div>
-
-        )
+  const onLeaveFeedback = (propertyName) => {
+    switch (propertyName) {
+      case 'good':
+        return setGood((prev) => prev + 1);
+      case 'neutral': 
+        return setNeutral((prev) => prev + 1);
+      case 'bad': 
+        return setBad((prev) => prev + 1);
+      default:
+        return;
     }
+  }
+
+  return (
+    <div>
+      <Section title="Please leave your feedback:">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={onLeaveFeedback} />
+      </Section>
+      <Section title="Statistics:">
+        {total ?
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positiveFeedback={countPositive()} /> :
+            <Notification message="There is no feedback yet." />}
+        </Section>
+      </div>
+            )
 }
